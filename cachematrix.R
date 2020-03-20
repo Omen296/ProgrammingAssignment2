07f5be5b9ef9dfa740e46ae0edd4bc18f2f6f21d
Omen296 <omen.of.the.damned@gmail.com>
2020-03-20 18:36:58 -0300

    Seems like I bungled the grammar because the example work. It needs a condition to check if the matrix changed and I need to figure out how it works properly but it checks out.

--- cachematrix.R
+++ cachematrix.R
@@ -3,18 +3,18 @@
 
 ## Creates a list with a matrix and an space for its inverse in cache to be called after
 
-makeCacheMatrix <- function(x = matrix())
-  {
+makeCacheMatrix <- function(x = matrix()){
   inv=NULL
-  setdata<-function(y)          {
-    x<<-y
-    m<<-NULL
+  ##THE PROBLEM STARTS HERE, getdara, setinv and getinv end up being the functions instead of the data proper
+  setdata<-function(y)          {    #Creates a vector that has the data of x and m  
+    x<<-y                            #And sends those values to another enviroment
+    inv<<-NULL
     }
-  getdata<-function () x
-  setinv <- function(solve) m <<- solve
-  getinv <- function() m
+  getdata<-function () x                           
+  setinv <- function(solve) inv <<- solve   
+  getinv <- function() inv
   list(setdata = setdata, getdata = getdata, setinv = setinv,
-       getinv = getinv)
+       getinv = getinv)  #  We create a list to get them in cacheSolve
   }
 
 
@@ -26,18 +26,20 @@ cacheSolve <- function(x, ...) {
       message("Getting inverse matrix from cache") #And tells you so
       return(inv)
     }
-    inv<-solve(x$regdata) #Produce the inverse and set it as a cached value
+    inv<-solve(x$getdata) #Produce the inverse and set it as a cached value
     x$getinv(inv) #Sets inv in the matrix
     inv #Prints the value
   }
+
+
 
 d<-c(2,3)
 e<-c(4,3)
 z<-data.frame(d, e)
 solve(z)
-makeCacheMatrix(z)
-cacheSolve(z)
-solve(z)==cacheSolve(z)
+D<-makeCacheMatrix(z)
+P<-cacheSolve(D)
+solve(z)==P
 
 
 
